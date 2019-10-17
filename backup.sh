c9ee3ffbd948d25d18cb107fda0c33b17019d1e4
www-data <koen.veys@protonmail.com>
2019-10-17 17:15:25 +0200

    remove tar.gz after successfull upload to S3 bucket

--- backup.sh
+++ backup.sh
@@ -75,17 +75,35 @@ for host in $hosts2backup; do
 	#zipping TAR file
 	$gzip $dir2bu2/`echo $host`_$timestamp.tar
 	gzipresult=$?
-		if [ "$gzipresult" == 0 ];then
-			echo -e "`now` GZIP: gzipped TARfile successfully" | tee -a $logfile
-		else
-			echo -e "`now` GZIP: gzipped TARfile NOT! successfully" | tee -a $logfile
-		fi
+	if [ "$gzipresult" == 0 ];then
+		echo -e "`now` GZIP: gzipped TARfile successfully" | tee -a $logfile
+	else
+		echo -e "`now` GZIP: gzipped TARfile NOT! successfully" | tee -a $logfile
+	fi
 
 	#uploading TAR.GZ file
 	$s3 -c $s3cfg put $dir2bu2/`echo $host`_$timestamp.tar.gz s3://$s3bucket
 	s3result=$?
-		if [ "$s3result" == 0 ];then
-			echo -e "`now` S3: uploaded TAR.GZfile successfully" | tee -a $logfile
+	if [ "$s3result" == 0 ];then
+		echo -e "`now` S3: uploaded TAR.GZfile successfully" | tee -a $logfile
+		rm -f $dir2bu2/`echo $host`_$timestamp.tar.gz
+		rmresult=$?
+			if [ "$rmresult" == 0 ];then
+				echo -e "`now` RM: TAR.GZfile successfully removed from $dir2bu2" | tee -a $logfile
+			else
+				echo -e "`now` RM: TAR.GZfile NOT! successfully removed from $dir2bu2" | tee -a $logfile
+				echo -e "`now` RM: TAR.GZfile is still in $dir2bu2" | tee -a $logfile
+			fi
+	else
+		echo -e "`now` S3: uploaded TAR.GZfile NOT! successfully" | tee -a $logfile
+		echo -e "`now` S3: TAR.GZfile is still in $dir2bu2" | tee -a $logfile
+	fi
+
+done
+
+echo -e "`now`;script end" | tee -a $logfile
+echo -e "==================================================================="  | tee -a $logfile
+			echof $dir2bu2/`echo $host`_$timestamp.tar.gz
 		else
 			echo -e "`now` S3: uploaded TAR.GZfile NOT! successfully" | tee -a $logfile
 		fi
