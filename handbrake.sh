c99c95f5736a2645e5d1afabe9746c1a16ddc14a
kveys <50111461+kveys@users.noreply.github.com>
2019-11-24 13:15:02 +0100

    Create handbrake.sh

---
+++ handbrake.sh
@@ -0,0 +1,6 @@
+#create script to automate HandBrake actions
+# we want to automate:
+# * creating directories for series
+# eg: season 1 - 10
+# mkdir disk 1 - 3
+# using: for season in $(seq 1 10); do mkdir "season $season"; done
